7f0d3cd181f3461f368da1d4b7771915e61bc932
Xavhyatt <xavier.hyatt@academytrainee.com>
2018-11-13 14:31:01 +0000

    Basics

---
+++ testPython/Loops.py
@@ -0,0 +1,17 @@
+student_names = ["James", "Katarina", "Ahri", "Zed", "Talon", "Ekko", "Ori"]
+
+# Testing break function
+
+for name in student_names:
+    if name == "Ekko":
+        print ("Found Them! " + name)
+        break
+    print("Currently testing " + name)
+
+# Testing Continue function
+for name in student_names:
+    if name == "Zed":
+        print ("Found Them! " + name)
+        continue
+    print("Currently testing " + name)
+
